815715107e55eece36dda801c5ada99970df75aa
Ciobanu Boris <b.ciobanu@mail.ru>
2022-11-07 16:49:17 +0000

    add

--- src/components/pages/main/Main.jsx
+++ src/components/pages/main/Main.jsx
@@ -6,8 +6,18 @@ import Portofolios from "../portofolios/Portofolios";
 import Experience from "../experience/Experience";
 import Education from "../education/Education";
 import Footer from "../footer/Footer";
+import { hotjar } from "react-hotjar";
 
 export default function Main() {
+  hotjar.initialize(
+    process.env.REACT_APP_HOTJAR_ID,
+    process.env.REACT_APP_HOTJAR_VERSION
+  );
+  //------------------------------>
+  if (hotjar.initialized()) {
+    hotjar.identify("USER_ID", { userProperty: "value" });
+    // Add an event
+  }
   return (
     <div>
       <Header />
